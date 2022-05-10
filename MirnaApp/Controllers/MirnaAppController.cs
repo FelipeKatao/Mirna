a0f1c154cd39bb6946fc2aeb2a08c08b6281b5be
Felipe <cataofelipe@gmail.com>
2022-05-10 00:58:30 -0300

    created Mirna Api

---
+++ MirnaApp/Controllers/MirnaAppController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MirnaApp.controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class MirnaAppController : ControllerBase
+    {
+        [HttpGet]
+        public  string Get(){
+            return "Seja bem vindo a mirna";
+        }
+    }
+}
