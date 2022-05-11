1b4f7192c59be29d4b06221932d1a6eb8cd3ce93
Felipe <cataofelipe@gmail.com>
2022-05-11 00:37:28 -0300

    update project contaner files

--- MirnaApp/Controllers/TokenAuth.cs
+++ MirnaApp/Controllers/TokenAuth.cs
@@ -2,6 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MirnaApp.controllers
 {
+    [Route("/")]
+    [ApiController]
+    public class startBase :ControllerBase
+    {
+        [HttpGet]
+        public string Get(){
+            return "Welcome!! Please crete your Id Key in MirnaWeb.com ";
+        }
+    }
     [Route("/[controller]")]
     [ApiController]
     public class Token : ControllerBase
