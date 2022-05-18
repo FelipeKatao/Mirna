d4d6f0a0b067d3c3101b1de919eeb81011241de1
Felipe <cataofelipe@gmail.com>
2022-05-18 00:02:41 -0300

    Update MirnaController

--- MirnaApp/Controllers/MirnaController.cs
+++ MirnaApp/Controllers/MirnaController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
 using System.Collections.Generic;
 using Microsoft.Extensions.Caching.Memory;
 using model;
@@ -8,13 +9,18 @@ namespace MirnaApp.controllers
 {
     [Route("/Mirna")]
     [ApiController]
-    public class Mirna: ControllerBase
+    public class Mirna : ControllerBase
     {
         [HttpGet]
         public string Get()
         {
             return "Welcome!! Please crete your Id Key in MirnaWeb.com ";
         }
+        [HttpGet("{token}/{str}")]
+        public IActionResult Get(string token, string str)
+        {
+            Redirect("/[silvercon]/connect/"+token+"/"+str);
+            return Content(token);
+        }
     }
-
 }
